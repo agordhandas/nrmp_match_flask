37ae70653977d64af6cf36f3866a31c76486bb40
Ankit Gordhandas <agordhandas@gmail.com>
2017-01-01 11:05:23 -0800

    app -> application

--- application.py
+++ application.py
@@ -6,22 +6,22 @@ from helpers import form_generation as fg
 from match_algo import run_match
 
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.route('/')
+@application.route('/')
 def hello_world():
     return render_template('index.html', user='sss')
 
-@app.route('/get_value')
+@application.route('/get_value')
 def get_value():
     return "myAnkit"
 
-@app.route('/get_basic_info_schema')
+@application.route('/get_basic_info_schema')
 def get_basic_info_schema():
     return json.dumps(fg.generate_specialty_form())
 
-@app.route('/post_basic_info', methods=['POST'])
+@application.route('/post_basic_info', methods=['POST'])
 def post_basic_info():
     r = request.get_json()
     basic_info = json.loads(request.data)
@@ -29,20 +29,20 @@ def post_basic_info():
     return "Success!"
 
 
-@app.route('/post_rol', methods=['POST'])
+@application.route('/post_rol', methods=['POST'])
 def post_rol():
     rol = request.get_json()
     pickle.dump(eval(request.data), open('match_data/rank_order_list.p', 'wb'))
     return str(rol)
 
-@app.route('/post_program_rankings', methods=['POST'])
+@application.route('/post_program_rankings', methods=['POST'])
 def post_program_rankings():
     r = request.get_json()
     program_rankings = json.loads(request.data)
     #pickle.dump (program_rankings, open('match_data/program_rankings.p', 'wb'))
     return "Success!"
 
-@app.route('/get_program_schema', methods=['POST'])
+@application.route('/get_program_schema', methods=['POST'])
 def get_program_schema():
     r = request.get_json()
     programs = json.loads(request.data)
@@ -50,14 +50,14 @@ def get_program_schema():
     return json.dumps(fg.generate_program_form(programs))
 
 
-@app.route('/get_rol_schema', methods=['POST'])
+@application.route('/get_rol_schema', methods=['POST'])
 def get_rol_schema():
     r = request.get_json()
     field = json.loads(request.data)['specialty']
     print field
     return json.dumps(fg.generate_rol_form(field))
 
-@app.route ('/get_match_results', methods=['POST'])
+@application.route ('/get_match_results', methods=['POST'])
 def get_match_results():
     #basic_info = pickle.load (open ('match_data/basic_info.p', 'rb'))
     #alias = basic_info['alias']
@@ -79,5 +79,5 @@ def get_match_results():
     return match
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
 

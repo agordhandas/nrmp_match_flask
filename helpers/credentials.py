fd414bcd5560a478f715ee79a038398ad37d7def
Ankit Gordhandas <agordhandas@gmail.com>
2017-01-01 11:20:24 -0800

    cleanup

--- helpers/credentials.py
+++ helpers/credentials.py
@@ -1,1 +1,8 @@
 __author__ = 'AnkitAthos'
+
+creds = {'host': 'nrmp-flask-react.cmw9gihb3z0r.us-west-2.rds.amazonaws.com',
+            'port': 3306,
+            'user': 'agordhandas',
+            'password': 'nrmp_match',
+            'schema': 'match_db',
+        }
